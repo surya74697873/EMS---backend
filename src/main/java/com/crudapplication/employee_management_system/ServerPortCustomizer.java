9bda553334fcbabb86bc7ecda4762d2b7eb3e87b
surya74697873 <suryapandi74697873@gmail.com>
2025-05-06 07:01:05 +0530

    bugs cleared

--- src/main/java/com/crudapplication/employee_management_system/ServerPortCustomizer.java
+++ src/main/java/com/crudapplication/employee_management_system/ServerPortCustomizer.java
@@ -4,8 +4,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class ServerPortCustomizer implements WebSerSverFactoryCustomizer<ConfigurableServletWebServerFactory> {
+public class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
         String port = System.getenv("X_ZOHO_CATALYST_LISTEN_PORT");
